84e5bd1639a175b3dc072f2907c5582abe7d0614
Andrii <shandrij1@gmail.com>
2024-11-05 11:31:52 +0100

    delete line from db.js

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const { DocumentStore } = require("ravendb");
 
-// const store = new DocumentStore("http://localhost:8080", "payment-calculations");
 const store = new DocumentStore("http://127.0.0.1:8080", "payment-calculations");
 
 store.initialize();
